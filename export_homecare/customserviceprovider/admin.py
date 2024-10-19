9e90ea5588a57d9bdfe8f427b6b74b02f6c53501
MathewPeter <mathewpeter2025@mca.ajce.in>
2024-10-19 12:25:53 +0530

    fds

--- export_homecare/customserviceprovider/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Accessoriesbuy,Feedback
-
-admin.site.register(Accessoriesbuy)
-admin.site.register(Feedback)
--- export_homecare/employee/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import service
-from .models import service_provider
-# Register your models here.
-admin.site.register(service)
-admin.site.register(service_provider)
