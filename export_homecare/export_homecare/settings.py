b2f45a23e267b6dce0ec99ff9797d93ac22a7a9d
MathewPeter <mathewpeter2025@mca.ajce.in>
2024-10-25 21:04:18 +0530

    update onestafo;e

--- export_homecare/export_homecare/settings.py
+++ export_homecare/export_homecare/settings.py
@@ -217,6 +217,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
