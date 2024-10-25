6e63e91626c633fb8a6b68fc951bec0d68342977
MathewPeter <mathewpeter2025@mca.ajce.in>
2024-10-25 13:44:34 +0530

    test working

--- export_homecare/customerlogin/admin.py
+++ export_homecare/customerlogin/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Customer,Booking,BookingDate,Message
 
 admin.site.register(Customer)
+admin.site.register(Booking)
+admin.site.register(BookingDate)
+admin.site.register(Message)
